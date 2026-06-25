5a284c485e1625834601d5a7e8a34f6ba55e762a
Norbert Schnell <norbert@schnell.space>
2026-06-25 15:25:44 +0200

    first running example version

---
+++ scripts.js
@@ -0,0 +1,129 @@
+import stories from './stories.js';
+
+const chapterContainer = document.getElementById('chapters');
+const audio = new Audio();
+let currentlyPlaying = null;
+
+const chapters = new Map();
+
+for (let i = 0; i < stories.length; i++) {
+  const story = stories[i];
+  const storyId = `story-${i + 1}`;
+  const chapterTitle = story.chapter || "";
+  let chapterDiv = chapters.get(chapterTitle);
+
+  if (!chapterDiv) {
+    chapterDiv = document.createElement('div');
+    chapterDiv.classList.add('chapter');
+    chapterDiv.innerHTML = `<h2 class="chapter-title">${chapterTitle}</h2>`;
+    chapterContainer.appendChild(chapterDiv);
+
+    chapters.set(chapterTitle, chapterDiv);
+  }
+
+  const storyDiv = document.createElement('div');
+  storyDiv.id = storyId;
+  storyDiv.classList.add('story');
+  chapterDiv.appendChild(storyDiv);
+
+  let innerHTML = ``;
+  const direction = (i % 2 === 0) ? 'left' : 'right';
+
+  if (story.speakers) {
+    innerHTML += `<h3 class="story-speakers"><span class="story-id"></span>${story.speakers}</h3>`
+  }
+
+  innerHTML += `
+    <div class="audio-player">
+      <img class="audio-button" src="./images/play.png" />
+      <span class=audio-text>Abspielen</span>
+      <span class="audio-progress hidden"><span class="audio-bar"></span>
+    </div>`;
+
+  if (i < stories.length - 1) {
+    innerHTML += '<hr />';
+  }
+
+  storyDiv.innerHTML = innerHTML;
+
+  const audioButton = document.querySelector(`#${storyId} .audio-player .audio-button`);
+  const audioProgress = document.querySelector(`#${storyId} .audio-player .audio-progress`);
+  const audioBar = document.querySelector(`#${storyId} .audio-player .audio-bar`);
+  const audioText = document.querySelector(`#${storyId} .audio-player .audio-text`);
+
+  story.state = {
+    id: storyId,
+    button: audioButton,
+    progress: audioProgress,
+    bar: audioBar,
+    text: audioText,
+    playing: false,
+  };
+
+  function onPlayToggle() {
+    const story = stories[i];
+
+    if (currentlyPlaying !== story) {
+      if (currentlyPlaying !== null) {
+        stopPlaying(currentlyPlaying, true);
+      }
+
+      audio.src = `audio/${story.audiofile}`;
+      currentlyPlaying = story;
+    }
+
+    if (!story.state.playing) {
+      startPlaying(story);
+    } else {
+      stopPlaying(story)
+    }
+  }
+
+  audioButton.addEventListener('click', onPlayToggle);
+  audioText.addEventListener('click', onPlayToggle);
+
+  audioProgress.addEventListener('click', (e) => {
+    if (currentlyPlaying === story) {
+      const state = currentlyPlaying.state;
+      const rect = state.progress.getBoundingClientRect();
+      const seek = audio.duration * (e.pageX - rect.x) / rect.width;
+      audio.currentTime = seek;
+    }
+  });
+}
+
+function startPlaying(story) {
+  const state = story.state;
+  audio.play();
+  state.button.src = './images/pause.png';
+  state.progress.classList.remove('hidden');
+  state.text.classList.add('hidden');
+  state.playing = true;
+}
+
+function stopPlaying(story, hideProgress) {
+  const state = story.state;
+  audio.pause();
+  state.button.src = './images/play.png';
+  state.bar.style.width = 0;
+  state.playing = false;
+
+  if (hideProgress) {
+    state.progress.classList.add('hidden');
+    state.text.classList.remove('hidden');
+  }
+}
+
+audio.addEventListener('timeupdate', () => {
+  if (currentlyPlaying !== null) {
+    const state = currentlyPlaying.state;
+    const progress = audio.currentTime / audio.duration;
+    state.bar.style.width = `${100 * progress}%`;
+  }
+});
+
+audio.addEventListener('ended', () => {
+  if (currentlyPlaying !== null) {
+    stopPlaying(currentlyPlaying);
+  }
+});
---
+++ stories.js
@@ -0,0 +1,21 @@
+export default [{
+  chapter: 'Im Schwimmbad',
+  speakers: 'Peter & Gabi',
+  audiofile: 'example.mp3',
+}, {
+  chapter: 'Im Schwimmbad',
+  speakers: 'Hans',
+  audiofile: 'example.mp3',
+}, {
+  chapter: 'Auf dem Spielplatz',
+  speakers: 'Barbara',
+  audiofile: 'example.mp3',
+}, {
+  chapter: 'Auf dem Spielplatz',
+  speakers: 'Gabi & Helga',
+  audiofile: 'example.mp3',
+}, {
+  chapter: 'Auf dem Spielplatz',
+  speakers: 'Jacob & Jakob',
+  audiofile: 'example.mp3',
+}];
